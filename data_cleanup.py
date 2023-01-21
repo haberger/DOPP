22a1ab568b43a97479317ffa1f5582ebfd34e575
haberger <d.haberger@homtial.com>
2023-01-21 21:05:53 +0100

    fixed bug in cpi merge

--- data_cleanup.py
+++ data_cleanup.py
@@ -8,12 +8,13 @@ from region_merge import merge_region
 def return_rows_where_all_corruption_data_is_available(df):
     '''BE AWARE THAT ONLY ONE OF ti_cpi and ti_cpi_om IS AVAILABLE NEVER BOTH'''
 
-    corruption_col = ['bci_bci', 'ti_cpi', 'vdem_corr', 'vdem_execorr', 'vdem_jucorrdc', 'vdem_pubcorr', 'wbgi_cce']
+    corruption_col = ['bci_bci', 'ti_cpi', 'ti_cpi_om','vdem_corr', 'vdem_execorr', 'vdem_jucorrdc', 'vdem_pubcorr', 'wbgi_cce']
+    merge = ['bci_bci', 'merge', 'vdem_corr', 'vdem_execorr', 'vdem_jucorrdc', 'vdem_pubcorr', 'wbgi_cce']
+    df['merge'] = df['ti_cpi'].combine_first(df['ti_cpi_om'])
+    df = df.dropna(subset=merge, axis=0, how='any')
+    df = df.drop(columns=['merge'])
 
-    df_cpi_combined = df.copy()
-    df_cpi_combined['ti_cpi']=df['ti_cpi'].combine_first(df['ti_cpi_om'])
-    df_all_corruption_info_available = df_cpi_combined.dropna(subset=corruption_col, axis=0, how='any')
-    return df_all_corruption_info_available
+    return df
 
 def load_reduced_df():
     data_dir = 'data'
@@ -23,3 +24,7 @@ def load_reduced_df():
     df = merge_region(df)
     df_reduced = return_rows_where_all_corruption_data_is_available(df)
     return df_reduced
+
+
+if __name__ == "__main__":
+    load_reduced_df()
