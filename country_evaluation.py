fa37d09c41227d7f813a7c382b8ca4dc0af69fa5
Alina Godun <godun.lina@gmail.com>
2023-01-22 23:20:17 +0100

    temp

--- country_evaluation.py
+++ country_evaluation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 def get_rel_err_df(rf_bf, df, corr_cols, threshold):
     relative_errs = {}
@@ -31,4 +32,26 @@ def get_rel_err_df_stats(df):
     relative_errs_per_country_full = pd.concat([relative_errs_per_country, relative_errs_per_country_total])
     relative_errs_per_country_full = pd.DataFrame(relative_errs_per_country_full.groupby(level=['cname', 'corr_id', 'rel_err_status']).rel_err.sum())
     return relative_errs_per_country_full
+
+
+def get_plots(relative_errs, q_dict, corr_id):
+    plot_dict = {}
+    
+    dfs = {}
+    dfs['normal'] = relative_errs[(relative_errs.rel_err <= q_dict[corr_id]) & (relative_errs.corr_id == corr_id)]
+    dfs['outlier'] = relative_errs[(relative_errs.rel_err > q_dict[corr_id]) & (relative_errs.corr_id == corr_id)]
+    dfs['full'] = relative_errs[(relative_errs.corr_id == corr_id)]
+    
+    for key, df in dfs.items():
+        plot_dict[f'{key}_dist'] = px.histogram(df, x='rel_err', title=f'Distribution of relative errors for {corr_id}, {key} range')
+    
+    for key, df in dfs.items():
+        plot_dict[f'{key}_dist_region'] = px.histogram(df, x='rel_err', color='region', barmode='overlay', histnorm='probability',
+            title=f'Distribution of relative errors for {corr_id} (by region), {key} range')
+        
+    for key, df in dfs.items():
+        plot_dict[f'{key}_dist_sub_region'] = px.histogram(df, x='rel_err', color='sub-region', barmode='overlay', histnorm='probability',
+            title=f'Distribution of relative errors for {corr_id} (by sub-region), {key} range')
+    
+    return plot_dict
 
